fb8f89e07f93cc5fcba7be3faa21e6d82055c0b4
yeaitsju <101952337+yeaitsju@users.noreply.github.com>
2022-12-03 23:46:53 +0000

    Deploying to gh-pages from @ yeaitsju/blackentertainmentlocator@770ed16cab7d1a5a741cfd2ccad629ca51547f76 🚀

--- assets/details.4b506f95.js
+++ assets/details.4b506f95.js
@@ -1,4 +1,4 @@
-import"./modulepreload-polyfill.c7c6310f.js";function o(){var s=document.getElementById("MainImg"),i=document.getElementsByClassName("small-img");i[0].onclick=function(){s.src=i[0].src},i[1].onclick=function(){s.src=i[1].src},i[2].onclick=function(){s.src=i[2].src},i[3].onclick=function(){s.src=i[3].src}}function t(s){let i=`https://www.apitutor.org/yelp/v3/businesses/${s}`;fetch(i).then(l=>l.json()).then(l=>{console.log(l),m(l)})}function n(s){let i=`https://www.apitutor.org/yelp/v3/businesses/${s}/reviews`;fetch(i).then(l=>l.json()).then(l=>{console.log(l),r(l)})}function m(s){const i=`
+import"./modulepreload-polyfill.c7c6310f.js";function t(){var s=document.getElementById("MainImg"),i=document.getElementsByClassName("small-img");i[0].onclick=function(){s.src=i[0].src},i[1].onclick=function(){s.src=i[1].src},i[2].onclick=function(){s.src=i[2].src},i[3].onclick=function(){s.src=i[3].src}}function r(s){let i=`https://www.apitutor.org/yelp/v3/businesses/${s}`;fetch(i).then(l=>l.json()).then(l=>{console.log(l),a(l)})}function m(s){let i=`https://www.apitutor.org/yelp/v3/businesses/${s}/reviews`;fetch(i).then(l=>l.json()).then(l=>{console.log(l),d(l)})}function a(s){const i=`
     <section class="container sbusiness my-5 pt-5">
         <div class="row mt-5">
             <div class="col-lg-5 col-md-12 col-12">
@@ -25,13 +25,23 @@ import"./modulepreload-polyfill.c7c6310f.js";function o(){var s=document.getElem
                 <h3 class="py-4">${s.name}</h3>
                 
                 <h4>${s.location.address1}</h4>
-                <h4>${s.location.address2}</h4>
+               
                 <h4>${s.location.city+", "+s.location.state+" "+s.location.zip_code}</h4>
                 <h4>${s.display_phone}</h4>
                 <span id="business-reviews"> </span>
             </div>
           
         </div>
-    </section>`;document.querySelector("#business-detail").innerHTML=i,o(),n(c)}function r(s){console.log(s);const l=`
-                <span>${s.reviews[0].text}</span>
-            `;document.querySelector("#business-reviews").innerHTML=l}const e=new Proxy(new URLSearchParams(window.location.search),{get:(s,i)=>s.get(i)});let c="6lPfM2AJIC6CZddrTvFYnA";e.businessid&&(c=e.businessid);t(c);
+    </section>`;document.querySelector("#business-detail").innerHTML=i,t(),m(e)}function d(s){console.log(s);const i=s.reviews[0],l=s.reviews[1],o=s.reviews[2],n=`
+                <br>
+                <h3>CUSTOMER REVIEWS</h3>
+                <hr>
+                <br>
+                <span>1. ${i.text}</span>
+                <br>
+                <br>
+                <span>2. ${l.text}</span>
+                <br>
+                <br>
+                <span>3. ${o.text}</span>
+            `;document.querySelector("#business-reviews").innerHTML=n}const c=new Proxy(new URLSearchParams(window.location.search),{get:(s,i)=>s.get(i)});let e="6lPfM2AJIC6CZddrTvFYnA";c.businessid&&(e=c.businessid);r(e);
