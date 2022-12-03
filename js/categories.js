af43566e66bbd72f0cb26c5ee8d25f552ff80278
Adwaina <arichard02@aol.com>
2022-12-03 16:09:30 -0500

    add page scroll button

--- js/categories.js
+++ js/categories.js
@@ -37,3 +37,41 @@ function setCategoryLinks(city) {
 // When the page first loads go find the location select menu
 // whatever is currently selected set all links to that location
 setCategoryLinks(document.querySelector("#location").value);
+
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top");
+const pageProgressBar = document.querySelector(".progress-bar");
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+    document.body.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  document.addEventListener("scroll", () => {
+    console.log("Scroll Height: ", scrollContainer().scrollHeight);
+    console.log("Client Height: ", scrollContainer().clientHeight);
+  
+    const scrolledPercentage =
+      (scrollContainer().scrollTop /
+        (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+      100;
+
+      pageProgressBar.style.width = `${scrolledPercentage}%`;
+
+      if (scrollContainer().scrollTop > showOnPx) {
+        backToTopButton.classList.remove("hidden");
+      } else {
+        backToTopButton.classList.add("hidden");
+      }
+    });
+ 
+
+      backToTopButton.addEventListener("click", goToTop);
+
+// When the page first loads go find the location select menu 
+// whatever is currently selected set all links to that location
+setCategoryLinks(document.querySelector("#location").value);
